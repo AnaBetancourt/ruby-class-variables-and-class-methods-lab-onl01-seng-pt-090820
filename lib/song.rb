13b1fddea0d31e04aeba1c7714c79da1052668a2
Ana Betancourt <anaa.betancourt13@gmail.com>
2020-09-26 00:23:27 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,49 @@
+require 'pry'
+
+class Song 
+  attr_accessor :name, :artist, :genre 
+  
+  @@count = 0 
+  @@artists = []
+  @@genres = []
+  
+  def initialize (name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    
+    @@count +=1 
+    @@artists << artist
+    @@genres << genre
+  end 
+  
+  def self.count
+    @@count 
+  end
+  
+  def self.artists
+    binding.pry
+    @@artists.each.do |name|
+      if @@artists = [name]
+        #do nothing
+      else
+        @@artists << name
+      end
+    end
+  
+  def self.genres
+    # if #already has genre in array
+    #   #do nothing
+    # else 
+    #   #add name to array
+    # end
+  end 
+  
+  def self.genre_count
+  end
+  
+  def self.artist_count
+  end
+
+  
+end
